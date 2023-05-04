8d374b149d6635f0756e9416c816bd01436e2df8
Beybut Abdulragimov <31037730+BeyCoder@users.noreply.github.com>
2023-05-05 02:54:46 +0400

    Update TxForm.tsx

--- src/components/TxForm/TxForm.tsx
+++ src/components/TxForm/TxForm.tsx
@@ -2,7 +2,7 @@ import React, {useCallback, useState} from 'react';
 import ReactJson from 'react-json-view';
 import './style.scss';
 import {useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
-import {Address, toNano} from "ton-core";
+import {Address, beginCell, toNano} from "ton-core";
 import {JettonMaster, TonClient} from "ton";
 
 const defaultTx = {
@@ -27,14 +27,27 @@ export function TxForm() {
 		const masterContract = JettonMaster.create(jetton);
 		const master = client.open(masterContract);
 
+
+		const contractAddress = Address.parse("EQDtbJhnJjdxYR8l4C1rQra_kzPR8_sBvtk0TonNxE5jAz6S");
 		master.getWalletAddress(Address.parseRaw(wallet.account.address)).then(addr => {
+			const message = beginCell()
+				.storeUint(0xf8a7ea5, 32)
+				.storeUint(0, 64)
+				.storeCoins(10 * Math.pow(10, 5))
+				.storeAddress(contractAddress)
+				.storeAddress(contractAddress)
+				.storeUint(0, 1)
+				.storeCoins(toNano("0.1"))
+				.storeUint(0, 1) // or 1
+				.endCell();
+
 			const newTx = {
 				validUntil: Date.now() + 1000000,
 				messages: [
 					{
 						address: addr.toString(),
 						amount: toNano("0.15").toString(),
-						payload: ""
+						payload: message.toBoc().toString("base64")
 					},
 				],
 			};
